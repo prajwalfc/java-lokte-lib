f56f76cb08ea1300e1e0d362941a26b9571345a8
prajwalfc <prajwalfc@gmail.com>
2025-12-06 22:43:27 -0500

    refactor: enhance MapCollectionTest with list and set collection methods

--- src/test/java/util/MapCollectionTest.java
+++ src/test/java/util/MapCollectionTest.java
@@ -1,13 +1,40 @@
 package util;
 
 
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 class MapCollectionTest {
 
-    @org.junit.jupiter.api.Test
-    void addToCollection() {
+    @Test
+    void addToCollectionList() {
+
+        MapCollection<String, List<String>, String> mapCollection = new MapCollection<>(ArrayList::new);
+        mapCollection.addToCollection("Nepal", "Kathmandu");
+        mapCollection.addToCollection("Nepal", "Kathmandu");
+        mapCollection.addToCollection("Nepal", "Pokhara");
+        mapCollection.addToCollection("India", "Delhi");
+        mapCollection.addToCollection("India", "Delhi");
+        mapCollection.addToCollection("India", "Chennai");
+        assert mapCollection.size() == 2;
+        assert mapCollection.get("Nepal").size() == 3;
+        assert mapCollection.get("India").size() == 3;
     }
 
-    @org.junit.jupiter.api.Test
-    void addAllToCollection() {
+    @Test
+    void addAllToCollectionSet() {
+        MapCollection<String, Set<String>, String> mapCollection = new MapCollection<>(HashSet::new);
+        mapCollection.addToCollection("Nepal", "Kathmandu");
+        mapCollection.addToCollection("Nepal", "Kathmandu");
+        mapCollection.addToCollection("Nepal", "Pokhara");
+        mapCollection.addToCollection("India", "Delhi");
+        mapCollection.addToCollection("India", "Delhi");
+        mapCollection.addToCollection("India", "Chennai");
+        assert mapCollection.size() == 2;
+        assert mapCollection.get("Nepal").size() == 2;
+        assert mapCollection.get("India").size() == 2;
     }
 }
